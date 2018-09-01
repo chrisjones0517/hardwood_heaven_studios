0ec92c182aa3f6e2ecca4c44b3ed9ead93a8e3f0
Chris Jones <chrisjones0517@gmail.com>
2018-08-31 23:42:16 -0500

    added contact form

--- public/js/index.js
+++ public/js/index.js
@@ -233,7 +233,7 @@ $(document).ready(function () {
     $('.card-img-top').on('click', function() {
         const imgSrc = $(this).attr('src');
         $('#image-modal-body').empty().append(`
-            <img class="image-zoom" src="${imgSrc}" width="795">
+            <img class="image-zoom" src="${imgSrc}">
         `);
         $('#image-zoom-modal').modal('show');
         
